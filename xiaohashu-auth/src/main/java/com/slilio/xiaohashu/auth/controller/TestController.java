256943534e3c608f3cb94ac4a0332b473b7429af
nangongxiaoxin <bx_tianhuan@163.com>
2025-03-03 01:16:22 +0800

    202503030116

---
+++ xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.slilio.xiaohashu.auth.controller;
+
+import com.slilio.framework.common.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public Response<String> test(){
+        return Response.success("Hello, 犬小哈专栏");
+    }
+}
