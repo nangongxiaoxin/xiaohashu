b57a832971646f01e3e8af79568a15738453b4d2
nangongxiaoxin <bx_tianhuan@163.com>
2025-03-07 00:32:21 +0800

    202503070032

--- xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/domain/dataobject/UserDO.java
+++ xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/domain/dataobject/UserDO.java
@@ -13,7 +13,11 @@ import java.time.LocalDateTime;
 @Builder
 public class UserDO {
     private Long id;
+
     private String username;
+
     private LocalDateTime createTime;
+
     private LocalDateTime updateTime;
+
 }
