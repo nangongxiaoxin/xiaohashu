137c5e3d849130c12f86ece8733f545005c02ae6
nangongxiaoxin <bx_tianhuan@163.com>
2025-03-09 02:00:05 +0800

    202503090200

--- xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/controller/TestController.java
@@ -2,28 +2,25 @@ package com.slilio.xiaohashu.auth.controller;
 
 import com.slilio.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.slilio.framework.common.response.Response;
+import java.time.LocalDateTime;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    @ApiOperationLog(description = "测试接口")
-    public Response<User> test(){
-        return Response.success(User.builder()
-                .nickName("犬小哈")
-                .createTime(LocalDateTime.now())
-                .build());
-    }
+  @GetMapping("/test")
+  @ApiOperationLog(description = "测试接口")
+  public Response<User> test() {
+    return Response.success(User.builder().nickName("犬小哈").createTime(LocalDateTime.now()).build());
+  }
 
-    @PostMapping("/test2")
-    @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2(@RequestBody User user){
-        return Response.success(user);
-    }
+  @PostMapping("/test2")
+  @ApiOperationLog(description = "测试接口2")
+  public Response<User> test2(@RequestBody @Validated User user) {
+    return Response.success(user);
+  }
 }
--- xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/controller/User.java
+++ xiaohashu-auth/src/main/java/com/slilio/xiaohashu/auth/controller/User.java
@@ -1,24 +1,21 @@
 package com.slilio.xiaohashu.auth.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
-    /**
-     * 昵称
-     */
-    private String nickName;
+  /** 昵称 */
+  @NotBlank(message = "昵称不能为空")
+  private String nickName;
 
-    /**
-     * 创建时间
-     */
-    private LocalDateTime createTime;
+  /** 创建时间 */
+  private LocalDateTime createTime;
 }
