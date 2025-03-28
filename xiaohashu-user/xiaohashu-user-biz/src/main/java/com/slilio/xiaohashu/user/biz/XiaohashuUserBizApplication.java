960e144ff190b21808edcfeb6dfcc90857fc31b6
nangongxiaoxin <bx_tianhuan@163.com>
2025-03-28 23:51:25 +0800

    202503282351

---
+++ xiaohashu-user/xiaohashu-user-biz/src/main/java/com/slilio/xiaohashu/user/biz/XiaohashuUserBizApplication.java
@@ -0,0 +1,11 @@
+package com.slilio.xiaohashu.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohashuUserBizApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(XiaohashuUserBizApplication.class, args);
+  }
+}
